0825dbb468fc1a58ab939b4c24f4ba7d3cd7cc85
R0MASIK21 <barmenbarli@gmail.com>
2026-07-07 23:55:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const tmi = require('tmi.js');
 const fs = require('fs');
 const http = require('http');
 
+// 1. ЗАВАНТАЖУЄМО БАЗУ НА САМОМУ ПОЧАТКУ
+let db = fs.existsSync('db.json') ? JSON.parse(fs.readFileSync('db.json', 'utf8')) : {};
+function saveDb() { fs.writeFileSync('db.json', JSON.stringify(db, null, 2)); }
+
+// 2. ВЕБ-СЕРВЕР, ЯКИЙ ПОКАЗУЄ ТОБІ СПИСОК БАЛІВ
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Бот працює!');
+    res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+    // Виводить всю базу db на екран!
+    res.end(JSON.stringify(db, null, 2));
 }).listen(process.env.PORT || 3000);
 
+// 3. ПІДКЛЮЧЕННЯ ДО ТВІЧА
 const client = new tmi.Client({
     identity: { username: 'r0masik_bot', password: 'oauth:v8wefxwrmrp9aee3774ogexiijsl6l' },
     channels: [ 'r0masik_' ]
 });
-
-let db = fs.existsSync('db.json') ? JSON.parse(fs.readFileSync('db.json', 'utf8')) : {};
-function saveDb() { fs.writeFileSync('db.json', JSON.stringify(db, null, 2)); }
 
 client.connect().catch(console.error);
 
@@ -25,19 +29,19 @@ client.on('message', async (channel, tags, message, self) => {
     const command = args[0].toLowerCase();
     const isMod = tags.mod || (tags.badges && tags.badges.broadcaster === '1');
 
-    // 1. АВТО-НАРАХУВАННЯ
+    // АВТО-НАРАХУВАННЯ
     if (!message.startsWith('!')) {
         if (!db[sender]) db[sender] = 0;
         db[sender] += 1;
         saveDb();
     }
 
-    // 2. СИСТЕМНІ КОМАНДИ (Бали, Рулетка, Топ, Дати, Розіграш, Го, На)
+    // СИСТЕМНІ КОМАНДИ 
     if (['!бали', '!рулетка', '!топ', '!дати', '!розіграш', '!го', '!на'].includes(command)) {
         require('./system_commands.js')(client, channel, sender, command, args, db, saveDb, isMod);
     }
     
-    // 3. ФАН-КОМАНДИ (Пеніс, Вкрасти, Пиво)
+    // ФАН-КОМАНДИ 
     if (['!пеніс', '!вкрасти', '!пиво'].includes(command)) {
         require('./fun_commands.js')(client, channel, sender, command, args, db, saveDb);
     }
