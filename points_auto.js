df44fb0adfb975d331867c8ba3f3d473dfa12f26
R0MASIK21 <barmenbarli@gmail.com>
2026-07-08 15:49:41 +0300

    Update points_auto.js

--- points_auto.js
+++ points_auto.js
@@ -1,4 +1,3 @@
-JavaScript
 module.exports = function(sender, message, db, saveDb, isBroadcaster) {
     if (message.startsWith('!')) return;
     if (isBroadcaster || ["streamelements", "streamlabs", "nightbot", "moobot", "r0masik_bot"].includes(sender)) return;
